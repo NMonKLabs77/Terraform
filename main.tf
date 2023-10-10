092edfae5331e8dff0086e9cc68e04a09617d1f4
Nehemiah Monrose <139259756+NMonKLabs77@users.noreply.github.com>
2023-10-10 18:58:27 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -26,42 +26,6 @@ resource "aws_instance" "jenkins" {
   }
 }
 
-# create security groups
-
-resource "aws_security_group" "web_ssh" {
-  description = "open ssh traffic"
-
-
-  ingress {
-    from_port = 22
-    to_port = 22
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-
-  }
-
-  ingress {
-    from_port = 8080
-    to_port = 8080
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-
-  }
-
-  egress {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    "Name" : "dep4-sg"
-  }
-
-}
-
-
 # Output the IP address of the EC2 instance to the terminal.
 output "jenkins_ip" {
   value = aws_instance.jenkins.public_ip
